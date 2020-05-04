01eb3b739b91c5bb3a5420242fcbdc3311d8d977
kuzja086 <kozs@tlink.ru>
2020-05-04 17:19:23 +0500

    1

--- vars/getParametrValue.groovy
+++ vars/getParametrValue.groovy
@@ -30,8 +30,8 @@ def getDefaultParams(){
         'platform1C' : '8.3.14.1779', //Версия платформы
         'xmlPath' : './xmlpath', // Путь к выгрузке файлов конфигурации
         'cfPath' : './build', // Каталог для выгрузкки *.cf и *.cfe
-        'saveExtensionInFile' : 'false' // Сохранить расширение в файл *.cfe
-        'extension' : "extension" // Имя расширения по умолчанию
+        'saveExtensionInFile' : 'false', // Сохранить расширение в файл *.cfe
+        'extension' : "extension", // Имя расширения по умолчанию
         'xmlPathExtension' : './xmlpathExtension', // Путь к выгрузке файлов расширения
 
         // EDT
