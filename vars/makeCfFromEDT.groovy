ec67257060e48041f8adc7d0241c000bb21a129e
kuzja086 <kozs@tlink.ru>
2020-05-05 15:26:27 +0500

    k

--- vars/makeCfFromEDT.groovy
+++ vars/makeCfFromEDT.groovy
@@ -2,9 +2,9 @@ def call(Map buildEnv){
     def connectionString = utils.getConnectionString(buildEnv)
 
     pipeline {
-        // agent {
-        //     label getParametrValue(buildEnv, 'agent')
-        // }
+        agent {
+            label getParametrValue(buildEnv, 'agent')
+        }
 
         post { // Выполняется после сборки
             failure {
@@ -26,73 +26,75 @@ def call(Map buildEnv){
             XMLPATHEXTENSION = getParametrValue(buildEnv, 'xmlPathExtension')
         }
 
-        stage('Инициализация') {
-            steps {
-                timestamps {
-                    script {
-                        dir(TEMP_CATALOG) {
-                            deleteDir()
-                        }
-                        dir(XMLPATH){
-                            deleteDir()
+        stages{
+            stage('Инициализация') {
+                steps {
+                    timestamps {
+                        script {
+                            dir(TEMP_CATALOG) {
+                                deleteDir()
+                            }
+                            dir(XMLPATH){
+                                deleteDir()
+                            }
                         }
                     }
                 }
             }
-        }
-        stage('Выгрузка проекта из EDT в файлы конфигурации') {
-            steps {
-                timestamps {
-                    script {
-                        PROJECT_NAME_EDT = "${CURRENT_CATALOG}\\${PROJECT_NAME}"
+            stage('Выгрузка проекта из EDT в файлы конфигурации') {
+                steps {
+                    timestamps {
+                        script {
+                            PROJECT_NAME_EDT = "${CURRENT_CATALOG}\\${PROJECT_NAME}"
 
-                        dumpProjectEDTInFiles(EDT_VERSION, TEMP_CATALOG, PROJECT_NAME_EDT, XMLPATH)
-                   }
+                            dumpProjectEDTInFiles(EDT_VERSION, TEMP_CATALOG, PROJECT_NAME_EDT, XMLPATH)
+                    }
+                    }
                 }
             }
-        }
-        stage('Загрузка конфигурации из файлов') {
-            steps {
-                timestamps {
-                    script {
-                        IB = "File=${TEMP_CATALOG}"
-
-                        utils.cmd("""
-                        cd /D C:\\Program Files (x86)\\1cv8\\${PLATFORM1C}\\bin\\
-                        1cv8.exe CREATEINFOBASE ${IB}
-                        1cv8.exe DESIGNER /WA- /DISABLESTARTUPDIALOGS /IBConnectionString ${IB} /LoadConfigFromFiles ${XMLPATH} /UpdateDBCfg
-                        """)
-                   }
-                }
-            }
-        }
-        stage('Сохранение файла .cf') {
-            steps {
-                timestamps {
-                    script {
-                        CFPATH = "${CFPATH}\\${PROJECT_NAME}.cf" 
-
-                        utils.cmd("""
-                        cd /D C:\\Program Files (x86)\\1cv8\\${PLATFORM1C}\\bin\\
-                        1cv8.exe DESIGNER /WA- /DISABLESTARTUPDIALOGS /IBConnectionString ${IB} /CreateDistributionFiles -cffile ${CFPATH}
-                        """)
-                   }
-                }
-            }
-        }
-         stage('Сохранение файла .cfe') {
-            steps {
-                timestamps {
-                    script {
-                        if (SAVEEXTENSIONINFILE.trim().equals('true')){
-                            PROJECT_NAME_EDT = "${CURRENT_CATALOG}\\${EXTENSION}"
-                            dumpProjectEDTInFiles(EDT_VERSION, TEMP_CATALOG, PROJECT_NAME_EDT, XMLPATHEXTENSION)
+            stage('Загрузка конфигурации из файлов') {
+                steps {
+                    timestamps {
+                        script {
+                            IB = "File=${TEMP_CATALOG}"
 
                             utils.cmd("""
                             cd /D C:\\Program Files (x86)\\1cv8\\${PLATFORM1C}\\bin\\
-                            1cv8.exe DESIGNER /WA- /DISABLESTARTUPDIALOGS /IBConnectionString ${IB} /LoadConfigFromFiles ${XMLPAthExtension} -Extension ${Extension} /UpdateDBCfg
-                            1cv8.exe DESIGNER /WA- /DISABLESTARTUPDIALOGS /IBConnectionString ${IB} /DumpCfg ${ExtensionPath} -Extension ${Extension}
+                            1cv8.exe CREATEINFOBASE ${IB}
+                            1cv8.exe DESIGNER /WA- /DISABLESTARTUPDIALOGS /IBConnectionString ${IB} /LoadConfigFromFiles ${XMLPATH} /UpdateDBCfg
                             """)
+                    }
+                    }
+                }
+            }
+            stage('Сохранение файла .cf') {
+                steps {
+                    timestamps {
+                        script {
+                            CFPATH = "${CFPATH}\\${PROJECT_NAME}.cf" 
+
+                            utils.cmd("""
+                            cd /D C:\\Program Files (x86)\\1cv8\\${PLATFORM1C}\\bin\\
+                            1cv8.exe DESIGNER /WA- /DISABLESTARTUPDIALOGS /IBConnectionString ${IB} /CreateDistributionFiles -cffile ${CFPATH}
+                            """)
+                    }
+                    }
+                }
+            }
+            stage('Сохранение файла .cfe') {
+                steps {
+                    timestamps {
+                        script {
+                            if (SAVEEXTENSIONINFILE.trim().equals('true')){
+                                PROJECT_NAME_EDT = "${CURRENT_CATALOG}\\${EXTENSION}"
+                                dumpProjectEDTInFiles(EDT_VERSION, TEMP_CATALOG, PROJECT_NAME_EDT, XMLPATHEXTENSION)
+
+                                utils.cmd("""
+                                cd /D C:\\Program Files (x86)\\1cv8\\${PLATFORM1C}\\bin\\
+                                1cv8.exe DESIGNER /WA- /DISABLESTARTUPDIALOGS /IBConnectionString ${IB} /LoadConfigFromFiles ${XMLPAthExtension} -Extension ${Extension} /UpdateDBCfg
+                                1cv8.exe DESIGNER /WA- /DISABLESTARTUPDIALOGS /IBConnectionString ${IB} /DumpCfg ${ExtensionPath} -Extension ${Extension}
+                                """)
+                            }
                         }
                     }
                 }
