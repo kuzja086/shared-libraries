eb6912f3dedf36e5a703810a9b5bb03b2c233f8b
kuzja086 <kozs@tlink.ru>
2020-05-17 20:45:12 +0500

    f

--- vars/testing.groovy
+++ vars/testing.groovy
@@ -198,7 +198,7 @@ def updateDbTask(platform1c, infobase, storage1cPath, storages1cCredentalsID, co
     stage("Загрузка из хранилища ${infobase}") {
         timestamps {
             prHelpers = new ProjectHelpers()
-            println storage1cPath != null
+            println storage1cPath == null
             println !storage1cPath.isEmpty()
             println storages1cCredentalsID != null
             println !storages1cCredentalsID.isEmpty()
