65dc140c84a1badf2bb834a6fef6c9b46eaf5a66
kuzja086 <kozs@tlink.ru>
2020-10-29 21:51:14 +0500

    1

--- src/io/libs/ProjectHelpers.groovy
+++ src/io/libs/ProjectHelpers.groovy
@@ -83,8 +83,8 @@ def dropDb(server1c, agentPort, base, base1CCredentialID, fulldrop = false) {
     withCredentials([usernamePassword(credentialsId: "${base1CCredentialID}", usernameVariable: 'USERNAMEBASE', passwordVariable: 'PASSWORDBASE')]){
         utils = new Utils()
         returnCode = utils.cmd("""
-            cd /D ${executorPath}
-            executor.cmd -s ./executor\\deleteBase.sbsl ${base} ${USERNAMEBASE} ${PASSWORDBASE}
+             cd /D ${executorPath}
+             executor.cmd -s ./executor\\deleteBase.sbsl ${base} ${USERNAMEBASE} ${PASSWORDBASE}
             """)
         if (returnCode != 0) { 
             error "error when deleting base with COM ${server1c}\\${base}. See logs above fore more information."
